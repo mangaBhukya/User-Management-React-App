cea9acdcdf863f351b63f2079832b2a4f929f254
Manga <mangab617@gmail.com>
2025-03-29 00:06:28 +0530

    Set up routing in App using React Router with LoginForm and UserList components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import LoginForm from './components/LoginForm';
-
+import React from "react";
+import LoginForm from "./components/LoginForm";
+import UserList from "./components/UserList";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-    <LoginForm/>
-    </div>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="/users" element={<UserList />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
