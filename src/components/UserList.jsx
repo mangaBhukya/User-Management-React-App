5950eefea1dd2ab4ae98ad07f5fbae256900775e
Manga <mangab617@gmail.com>
2025-03-29 13:16:02 +0530

    Implement server-side pagination for UserList

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -4,15 +4,24 @@ import { getUsers } from "../services/userApi";
 
 const UserList = () => {
   const [users, setUsers] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [pageSize, setPageSize] = useState(6);
 
-  const loadUsers = async () => {
-    const usersData = await getUsers();
-    console.log(usersData);
-    setUsers(usersData.data);
+  const loadUsers = async (page, pageSize) => {
+    try {
+      const usersData = await getUsers(page+1, pageSize);
+      console.log(usersData);
+      setUsers(usersData.data);
+      setTotalPages(usersData.total_pages);
+    } catch (error) {
+      console.log("error", error);
+      throw error;
+    }
   };
   useEffect(() => {
-    loadUsers();
-  });
+    loadUsers(page, pageSize);
+  }, [page, pageSize]);
 
   const columns = [
     {
@@ -37,16 +46,35 @@ const UserList = () => {
         );
       },
     },
-    { field: "first_name", headerName: "First Name", headerClassName: "bold-header", width: 90 },
-    { field: "last_name", headerName: "Last Name", headerClassName: "bold-header", width: 90 },
+    {
+      field: "first_name",
+      headerName: "First Name",
+      headerClassName: "bold-header",
+      width: 90,
+    },
+    {
+      field: "last_name",
+      headerName: "Last Name",
+      headerClassName: "bold-header",
+      width: 90,
+    },
   ];
 
+
   return (
-    <div> 
+    <div>
       <DataGrid
         rows={users}
         columns={columns}
         checkboxSelection
+        rowCount={totalPages * pageSize}
+        pageSizeOptions={[6]}
+        paginationMode="server"
+        onPaginationModelChange={(model) => {
+          setPage(model.page);
+          setPageSize(model.pageSize);
+        }}
+        paginationModel={{ page, pageSize }}
       />
     </div>
   );
