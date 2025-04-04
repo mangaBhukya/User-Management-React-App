d946f7a62dff99c1c12bf38373e1fbd7c72bcae2
Manga <mangab617@gmail.com>
2025-04-04 14:14:45 +0530

    replaces the current entry in the history stack

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,25 +1,15 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+// ProtectedRoute.jsx
+import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ element }) => {
-  const [isTokenValid, setIsTokenValid] = useState(true);
-  const navigate = useNavigate();
+  const token = localStorage.getItem("token");
 
-  const isTokenExpired = () => {
-    const token = localStorage.getItem("token");
-    if (!token) return true;
-    return false;
-  };
+  if (!token) {
+    localStorage.clear();
+    return <Navigate to="/" replace />;
+  }
 
-  useEffect(() => {
-    if (isTokenExpired()) {
-      localStorage.clear();
-      setIsTokenValid(false);
-      navigate("/");
-    }
-  }, [navigate]);
-
-  return isTokenValid ? element : null;
+  return element;
 };
 
 export default ProtectedRoute;
